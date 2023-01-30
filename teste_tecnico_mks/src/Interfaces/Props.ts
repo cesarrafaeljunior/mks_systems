884cd3b771f2196dbb8c7c64e5548bcc97ed5593
César Rafael <cesarrdev@gmail.com>
2023-01-30 18:28:01 -0300

    feat: add  button drecrement and increment

--- teste_tecnico_mks/src/Interfaces/Props.ts
+++ teste_tecnico_mks/src/Interfaces/Props.ts
@@ -1,8 +1,12 @@
+import { IProducts } from "./Products";
+
 export interface IPropsChildren {
   children: React.ReactNode;
   className?: string;
   imgProduct?: string;
   photo?: string;
+  updateProductIncrement?: () => IProducts | void;
+  updateProductDecrement?: () => IProducts | void;
 }
 
 export interface IPropsProductInformations {
---
+++ teste_tecnico_mks/src/services/cart/productsServices.ts
@@ -0,0 +1,63 @@
+import { IProducts } from "../../Interfaces/Products";
+
+const addProductInCart = (product: IProducts, itemsCart: IProducts[]) => {
+  const foundProduct = itemsCart.find((elem) => elem.id == product.id);
+
+  if (!foundProduct) {
+    const newProduct: IProducts = {
+      ...product,
+      amount: 1,
+    };
+    return newProduct;
+  }
+  console.log("Este item já está na lista");
+};
+
+const incrementQuantityProductInCart = (
+  product: IProducts,
+  itemsCart: IProducts[]
+): IProducts | undefined => {
+  const foundProductInCart: IProducts | undefined = itemsCart.find(
+    (elem) => elem.id == product.id
+  );
+
+  if (foundProductInCart) {
+    const updateProductFounded = {
+      ...foundProductInCart,
+      amount: (foundProductInCart.amount || 0) + 1,
+    };
+    return updateProductFounded;
+  }
+};
+
+const decrementQuantityProductInCart = (
+  product: IProducts,
+  itemsCart: IProducts[]
+): IProducts | undefined => {
+  const foundProductInCart: IProducts | undefined = itemsCart.find(
+    (elem) => elem.id == product.id
+  );
+
+  if (foundProductInCart) {
+    const updateProductFounded = {
+      ...foundProductInCart,
+      amount: (foundProductInCart.amount || 0) - 1,
+    };
+
+    return updateProductFounded;
+  }
+};
+
+const removeProductInCart = (id: string, itemsCart: IProducts[]) => {
+  const foundProductInCart: IProducts | undefined = itemsCart.find(
+    (elem) => elem.id == id
+  );
+  return foundProductInCart;
+};
+
+export {
+  addProductInCart,
+  incrementQuantityProductInCart,
+  decrementQuantityProductInCart,
+  removeProductInCart,
+};
